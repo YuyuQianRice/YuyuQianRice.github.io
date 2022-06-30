51a67402ca168faeb82b2fa9921c46ed1052c9aa
Yuyu Qian <Yuyu.Qian.Rice@gmail.com>
2022-06-29 23:01:07 -0700

    Updates

---
+++ precache-manifest.bb257547f576a55628a6f42e67056b9a.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbe83dc4aa9568e0189e",
+    "url": "/YuyuQianRice.github.io/static/css/main.4243b554.chunk.css"
+  },
+  {
+    "revision": "bbe83dc4aa9568e0189e",
+    "url": "/YuyuQianRice.github.io/static/js/main.bbe83dc4.chunk.js"
+  },
+  {
+    "revision": "e4c123256ec82141fcb1",
+    "url": "/YuyuQianRice.github.io/static/js/1.e4c12325.chunk.js"
+  },
+  {
+    "revision": "829b8ae29ee31bbcd295",
+    "url": "/YuyuQianRice.github.io/static/css/2.c444f670.chunk.css"
+  },
+  {
+    "revision": "829b8ae29ee31bbcd295",
+    "url": "/YuyuQianRice.github.io/static/js/2.829b8ae2.chunk.js"
+  },
+  {
+    "revision": "09faa739bb9f7e720a0f",
+    "url": "/YuyuQianRice.github.io/static/js/runtime~main.09faa739.js"
+  },
+  {
+    "revision": "dc0f2f51489a52dfade91489c798e430",
+    "url": "/YuyuQianRice.github.io/static/media/2B.dc0f2f51.svg"
+  },
+  {
+    "revision": "4e2767e57080879d655f1d00eb0d0a57",
+    "url": "/YuyuQianRice.github.io/static/media/2C.4e2767e5.svg"
+  },
+  {
+    "revision": "de33bdd7808262b43dc773af7df6562a",
+    "url": "/YuyuQianRice.github.io/static/media/2D.de33bdd7.svg"
+  },
+  {
+    "revision": "99d3ae9ec6a482128a49b34c33091ed8",
+    "url": "/YuyuQianRice.github.io/static/media/2H.99d3ae9e.svg"
+  },
+  {
+    "revision": "57459f5d10a8f27301b9164294597fa4",
+    "url": "/YuyuQianRice.github.io/static/media/2S.57459f5d.svg"
+  },
+  {
+    "revision": "ca35549453c43a1c9492012d635c72a3",
+    "url": "/YuyuQianRice.github.io/static/media/3C.ca355494.svg"
+  },
+  {
+    "revision": "a2d4020ab516afb674efae27e44c5225",
+    "url": "/YuyuQianRice.github.io/static/media/3D.a2d4020a.svg"
+  },
+  {
+    "revision": "f08b57dad6fe43d4f260ffec7ef8221d",
+    "url": "/YuyuQianRice.github.io/static/media/3H.f08b57da.svg"
+  },
+  {
+    "revision": "f1399ed4e4ce8c78dafba2b04104b3ef",
+    "url": "/YuyuQianRice.github.io/static/media/3S.f1399ed4.svg"
+  },
+  {
+    "revision": "7a83f06c52762f548213a7a1d431f126",
+    "url": "/YuyuQianRice.github.io/static/media/4C.7a83f06c.svg"
+  },
+  {
+    "revision": "79d03eb7072847b85d0d994de0cd5e86",
+    "url": "/YuyuQianRice.github.io/static/media/4D.79d03eb7.svg"
+  },
+  {
+    "revision": "df33d8bc93f9685f903dddbb95f92a3d",
+    "url": "/YuyuQianRice.github.io/static/media/4H.df33d8bc.svg"
+  },
+  {
+    "revision": "9b9a27a628a6f87ff9f68b2be6af69f0",
+    "url": "/YuyuQianRice.github.io/static/media/4S.9b9a27a6.svg"
+  },
+  {
+    "revision": "7dd628361c226d48b5b8671d087436c1",
+    "url": "/YuyuQianRice.github.io/static/media/5C.7dd62836.svg"
+  },
+  {
+    "revision": "7f6dc8c68981872e4064e466bbc10ff5",
+    "url": "/YuyuQianRice.github.io/static/media/5D.7f6dc8c6.svg"
+  },
+  {
+    "revision": "0f915ac47b36829c7aa7b2c2917bff7f",
+    "url": "/YuyuQianRice.github.io/static/media/5H.0f915ac4.svg"
+  },
+  {
+    "revision": "1c6bf310b8bb418aad872f5de077185c",
+    "url": "/YuyuQianRice.github.io/static/media/5S.1c6bf310.svg"
+  },
+  {
+    "revision": "6db350ab2b8e74ada96352bde700956f",
+    "url": "/YuyuQianRice.github.io/static/media/6C.6db350ab.svg"
+  },
+  {
+    "revision": "aca7e38a3c6a2d3067a263e0ac367908",
+    "url": "/YuyuQianRice.github.io/static/media/6D.aca7e38a.svg"
+  },
+  {
+    "revision": "f5bf172620cbe68871454ad8734f9507",
+    "url": "/YuyuQianRice.github.io/static/media/6H.f5bf1726.svg"
+  },
+  {
+    "revision": "4cb697e048cb8e40e7bfd50b29d11afe",
+    "url": "/YuyuQianRice.github.io/static/media/6S.4cb697e0.svg"
+  },
+  {
+    "revision": "15afb6ebde2d897f83f97f7790b12c68",
+    "url": "/YuyuQianRice.github.io/static/media/7C.15afb6eb.svg"
+  },
+  {
+    "revision": "35969cb999f1b7e96e945045dd8571c0",
+    "url": "/YuyuQianRice.github.io/static/media/7D.35969cb9.svg"
+  },
+  {
+    "revision": "e7f31a9fa904ea28a26058cdec5c2675",
+    "url": "/YuyuQianRice.github.io/static/media/7H.e7f31a9f.svg"
+  },
+  {
+    "revision": "1c3cc9fae1cb22f7d1c5eb087f3384e4",
+    "url": "/YuyuQianRice.github.io/static/media/7S.1c3cc9fa.svg"
+  },
+  {
+    "revision": "6cfc4531e3b9eb77c8b66ecb239f6766",
+    "url": "/YuyuQianRice.github.io/static/media/8C.6cfc4531.svg"
+  },
+  {
+    "revision": "614e31778be21775662aa093a9767f52",
+    "url": "/YuyuQianRice.github.io/static/media/8D.614e3177.svg"
+  },
+  {
+    "revision": "f33d5f0870ca70c0518bbf404f75e541",
+    "url": "/YuyuQianRice.github.io/static/media/8H.f33d5f08.svg"
+  },
+  {
+    "revision": "1bc26b6294448df49c2ebc8faa250ab2",
+    "url": "/YuyuQianRice.github.io/static/media/8S.1bc26b62.svg"
+  },
+  {
+    "revision": "6544315a206eeb7d3ec560c49086b8a8",
+    "url": "/YuyuQianRice.github.io/static/media/9C.6544315a.svg"
+  },
+  {
+    "revision": "75f61e581abbf0057f774102f3a11712",
+    "url": "/YuyuQianRice.github.io/static/media/9D.75f61e58.svg"
+  },
+  {
+    "revision": "29ac006ec940fd85d1ae4dea22d5c247",
+    "url": "/YuyuQianRice.github.io/static/media/9H.29ac006e.svg"
+  },
+  {
+    "revision": "f454c76d275a2ec5b9f2a26da62c4773",
+    "url": "/YuyuQianRice.github.io/static/media/9S.f454c76d.svg"
+  },
+  {
+    "revision": "7f590e9ea8df13ec31b9c0f83bd59ec1",
+    "url": "/YuyuQianRice.github.io/static/media/AC.7f590e9e.svg"
+  },
+  {
+    "revision": "d9b5e18a19198548a43ddf7ad6c07d8e",
+    "url": "/YuyuQianRice.github.io/static/media/AD.d9b5e18a.svg"
+  },
+  {
+    "revision": "c782dbb9b068b0a8b15460f40364e498",
+    "url": "/YuyuQianRice.github.io/static/media/AH.c782dbb9.svg"
+  },
+  {
+    "revision": "a376918d4efc4a023ed46914912617dc",
+    "url": "/YuyuQianRice.github.io/static/media/AS.a376918d.svg"
+  },
+  {
+    "revision": "c8652f15f54133925d616e5103f4fc0e",
+    "url": "/YuyuQianRice.github.io/static/media/JC.c8652f15.svg"
+  },
+  {
+    "revision": "188bab3ba8084d5c0a0a6bcb8eae7bb6",
+    "url": "/YuyuQianRice.github.io/static/media/JD.188bab3b.svg"
+  },
+  {
+    "revision": "88bca5ff97d5f4d7631cb756b05e25fb",
+    "url": "/YuyuQianRice.github.io/static/media/JH.88bca5ff.svg"
+  },
+  {
+    "revision": "2cd03864407846c6f4fb80144a8b38b3",
+    "url": "/YuyuQianRice.github.io/static/media/JS.2cd03864.svg"
+  },
+  {
+    "revision": "8b228c056ee6d8d7aeed5c31f48c6854",
+    "url": "/YuyuQianRice.github.io/static/media/KC.8b228c05.svg"
+  },
+  {
+    "revision": "db5de2b174ff0e37114221596df71762",
+    "url": "/YuyuQianRice.github.io/static/media/KD.db5de2b1.svg"
+  },
+  {
+    "revision": "652739dfc57cd99f4e26dc3dd45b612b",
+    "url": "/YuyuQianRice.github.io/static/media/KH.652739df.svg"
+  },
+  {
+    "revision": "7006c4d0a72b6a55841ae79eeff45581",
+    "url": "/YuyuQianRice.github.io/static/media/KS.7006c4d0.svg"
+  },
+  {
+    "revision": "a2d9c2428e56992c2d32724edefcf53c",
+    "url": "/YuyuQianRice.github.io/static/media/QC.a2d9c242.svg"
+  },
+  {
+    "revision": "a3b7183d98c9231179a3291b255d4c03",
+    "url": "/YuyuQianRice.github.io/static/media/QD.a3b7183d.svg"
+  },
+  {
+    "revision": "cb4d8321b2d5041079f9b027d8f9adce",
+    "url": "/YuyuQianRice.github.io/static/media/QH.cb4d8321.svg"
+  },
+  {
+    "revision": "38219e8e65c82825f518e3b9b0e7c356",
+    "url": "/YuyuQianRice.github.io/static/media/QS.38219e8e.svg"
+  },
+  {
+    "revision": "cb892b652320c05832a58cf128a45e8d",
+    "url": "/YuyuQianRice.github.io/static/media/TC.cb892b65.svg"
+  },
+  {
+    "revision": "70d52a971416aa9135b2d0f8fea34cdb",
+    "url": "/YuyuQianRice.github.io/static/media/TD.70d52a97.svg"
+  },
+  {
+    "revision": "4793a1eef451b632a58ba98ccbc34a23",
+    "url": "/YuyuQianRice.github.io/static/media/TH.4793a1ee.svg"
+  },
+  {
+    "revision": "451822d6463ac226f853346ce83519c9",
+    "url": "/YuyuQianRice.github.io/static/media/TS.451822d6.svg"
+  },
+  {
+    "revision": "7c1c4634e64fb08da9d3f822e424e9b9",
+    "url": "/YuyuQianRice.github.io/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/YuyuQianRice.github.io/precache-manifest.bb257547f576a55628a6f42e67056b9a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/YuyuQianRice.github.io/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
